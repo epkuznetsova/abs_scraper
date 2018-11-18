5c5767e0350a227e20afa7e9b49c1f4587126a75
epkuznetsova <kuznetsovaek.p@gmail.com>
2018-11-18 18:24:13 +1100

    ABS downoloader files

---
+++ DownloaderParams.py
@@ -0,0 +1,42 @@
+catalogue_dictionary = {"3101.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=3.1",
+"3401.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=3.4",
+"5204.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.1",
+"5302.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.2",
+"5609.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.5",
+"5519.0.55.001":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.4",
+"6202.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=7.2",
+"6291.0.55.001":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=7.2",
+"6291.0.55.003":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=7.2",
+"6302.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=7.3",
+"6401.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=7.4",
+"8501.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=9.4",
+"8731.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=9.5",
+"8752.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=9.5",
+"8755.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=9.5",
+"8762.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=9.5",
+"8782.0.65.001":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=9.5",
+"8412.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=10.1",
+"5206.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.1",
+"5232.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.1",
+"5368.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.2",
+"5671.0":"http://www.abs.gov.au/AUSSTATS/abs@.nsf/ViewContent?readform&view=ProductsbyCatalogue&Action=Expand&Num=6.5"}
+
+
+section_dictionary = {
+"1": "General",
+"2": "Census of Population and Housing",
+"3": "Demography",
+"4": "Social Statistics",
+"5": "National Accounts, International Trade and Finance",
+"6": "Labour Statistics and Prices",
+"7": "Agriculture",
+"8": "Secondary Industry and Distribution",
+"9": "Transport"}
+
+
+import_folder = "..//import"
+archive_folder = '..//archive'
+processed_folder = '..//processed//abs'
+logging_folder = '..//logging'
+
+
